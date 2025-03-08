8cf422eb13b7fc315de1f5daf322331df420f11d
Sudip <sudipbhandari67@gmail.com>
2025-03-08 23:42:16 +0545

    feat: book record api

---
+++ BookRecordAPI/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookRecordAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Author { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Genre { get; set; }
+
+        [Range(1000, 2100, ErrorMessage = "Published Year must be between 1000 and 2100.")]
+        public int PublishedYear { get; set; }
+
+    }
+}
